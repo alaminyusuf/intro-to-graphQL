2acd66af38a83ed0373c5051f8ec83c64626f48e
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-06-08 10:03:56 +0100

    set some dummy text to test graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,12 @@ const graphql = require('graphql')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+]
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
